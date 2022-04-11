3de9a50657d22c6cc3dc1fcd88a57ac5955caea4
Omitsusan199411 <59048819+Omitsusan199411@users.noreply.github.com>
2022-04-12 01:55:16 +0900

    react-practice propsやstate機能を実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+// 関数コンポーネントでuseState()関数を使用するためにreactの中のuseStateをimportする
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert("Hi");
+  const onClickButtonCountUp = () => {
+    setNum(num + 1);
+  };
+  // stateの定義。コンポーネントが持つ可変的（動的な）な状態・データ
+  // 配列の分割代入。numはstateの変数名、setNumはstate(num)の状態を動的に変更（更新）するための関数
+  // useState(初期値)は関数コンポーネントでstateを管理するための機能。（管理とは保持、更新のこと）
+  // const [state, stateを更新する関数] = useState(初期値);で定義する。
+  const [num, setNum] = useState(0);
   // returnでJSX構文(HTML要素)を返す。複数のHTMLコードがある場合はreturnの後に()を付ける
   return (
     <>
@@ -13,7 +21,9 @@ const App = () => {
       {/* 下記はprops(引数のようなもの)をコンポーネント(ColorfulMessage)に渡す */}
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickButtonCountUp}>カウントアップ</button>
+      {/* numはuseStateの変数 */}
+      <p>{num}</p>
     </>
   );
 };
